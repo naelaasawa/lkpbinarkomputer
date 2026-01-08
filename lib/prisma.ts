082649eb379d2fdfbf68dcade367eedaa16e181b
Abyan Dimas <abyandimas@Abyans-MacBook-Air.local>
2026-01-08 13:48:05 +0700

    minor update 1

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,24 +1,19 @@
 import "dotenv/config";
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 import { PrismaClient } from './generated/prisma/client';
-import mysql from 'mysql2/promise';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const prismaClientSingleton = () => {
-    // Create the connection pool ONLY when creating the client
-    const pool = mysql.createPool({
-        host: process.env.DATABASE_HOST || 'localhost',
-        user: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        database: process.env.DATABASE_NAME,
-        connectionLimit: 5, // Safe limit for dev
-    });
+const adapter = new PrismaMariaDb({
+    host: process.env.DATABASE_HOST || 'localhost',
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    connectionLimit: 5
+});
 
-    const adapter = new PrismaMariaDb(pool as any);
-    return new PrismaClient({ adapter });
-}
-
-export const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+export const prisma =
+    globalForPrisma.prisma ||
+    new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
